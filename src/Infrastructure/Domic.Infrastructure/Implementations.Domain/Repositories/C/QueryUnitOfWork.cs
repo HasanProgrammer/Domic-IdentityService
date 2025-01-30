a7b82961fe04d92f3efe60116351482f524e6759
Hasan Karami <hasankarami2020313@gmail.com>
2025-01-30 07:17:22 +0330

    update

---
+++ src/Infrastructure/Domic.Infrastructure/Implementations.Domain/Repositories/C/QueryUnitOfWork.cs
@@ -0,0 +1,57 @@
+using System.Data;
+using Domic.Domain.Commons.Contracts.Interfaces;
+using Domic.Persistence.Contexts.C;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Domic.Infrastructure.Implementations.Domain.Repositories.C;
+
+public class CommandUnitOfWork : ICommandUnitOfWork
+{
+    private readonly SQLContext _context;
+
+    private IDbContextTransaction _transaction;
+
+    public CommandUnitOfWork(SQLContext context) => _context = context;
+
+    public void Transaction(IsolationLevel isolationLevel) 
+        => _transaction = _context.Database.BeginTransaction(isolationLevel); //Resource
+
+    public async Task TransactionAsync(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        _transaction = await _context.Database.BeginTransactionAsync(isolationLevel, cancellationToken); //Resource
+    }
+
+    public void Commit()
+    {
+        _context.SaveChanges();
+        _transaction.Commit();
+    }
+
+    public async Task CommitAsync(CancellationToken cancellationToken)
+    {
+        await _context.SaveChangesAsync(cancellationToken);
+        await _transaction.CommitAsync(cancellationToken);
+    }
+
+    public void Rollback() => _transaction.Rollback();
+
+    public Task RollbackAsync(CancellationToken cancellationToken)
+    {
+        if (_transaction is not null)
+            return _transaction.RollbackAsync(cancellationToken);
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose() => _transaction?.Dispose();
+
+    public ValueTask DisposeAsync()
+    {
+        if (_transaction is not null)
+            return _transaction.DisposeAsync();
+
+        return ValueTask.CompletedTask;
+    }
+}
