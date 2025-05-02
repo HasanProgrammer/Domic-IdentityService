a423ee5ce03875348c2f573d5f9af2fc75b55327
Hasan1375-tech <karami.samdad@gmail.com>
2025-05-02 22:41:20 +0330

    update

--- src/Presentation/Domic.WebAPI/Program.cs
+++ src/Presentation/Domic.WebAPI/Program.cs
@@ -1,5 +1,4 @@
 using Domic.Core.Infrastructure.Extensions;
-using Domic.Core.UseCase.Contracts.Interfaces;
 using Domic.Core.WebAPI.Extensions;
 using Domic.Infrastructure.Extensions.C;
 using Domic.Infrastructure.Extensions.Q;
